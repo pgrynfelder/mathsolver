eba57dfa2186794bc845ac9ef5b104617e2c4dc5
pitek1 <piotr.grynfelder@gmail.com>
2017-02-03 16:38:44 +0100

    old_getimage deleted due to causing errors

--- getimage.py
+++ getimage.py
@@ -3,27 +3,8 @@
 # requires android debugging enabled on the device and adb installed on the pc
 # adb easy install: https://forum.xda-developers.com/showthread.php?t=2588979
 import subprocess
-from datetime import datetime
 from re import search
     
-def _old_getimage(camerapath = "storage/emulated/0/dcim/camera"):
-    output = subprocess.check_output('adb shell ls "{0}" | grep "{1}"'.
-                                     format(camerapath,
-                                            datetime.
-                                            now().
-                                            strftime('%Y%m%d')))
-    # getting files only with today's date to prevent searching
-    # through thousands of strings
-    output = str(output)
-    output = output[2:-1:]
-    output = output.split('\\r\\n')
-    output = output[:-1:]
-    filename = (max(output, key = lambda x: x[13:19:]))
-    subprocess.run('adb pull "{0}/{1}" "cache.jpg"'.
-                   format(camerapath,
-                          filename))
-    # download the latest file to the pc
-
 def getimage(camerapath = "storage/emulated/0/dcim/camera"):
     try:
         output = subprocess.check_output('adb shell ls -t "{0}" | head -n 1'.
@@ -38,5 +19,5 @@ def getimage(camerapath = "storage/emulated/0/dcim/camera"):
     subprocess.run('adb pull "{0}/{1}" "cache.jpg"'.
                    format(camerapath, filename))
     
-if __name__ == '__main__':    
+if __name__ == '__main__':
     getimage()
